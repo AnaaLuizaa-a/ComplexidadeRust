5465bb796fcfe3b7d38c2cfca5e2eef5038571f8
Ana Luiza <analuizamattos884@gmail.com>
2026-03-21 11:49:25 -0300

    Exercicio 2

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-pub fn verificar_primeiro(lista: &[i32]) -> Option<i32>{
+pub fn somar_lista(lista: &[i32]) -> Option<i32>{
     if lista.is_empty() {
-        return None;
+        None
+    
+    } else {
+        let mut soma = 0;
+        for numero in lista {
+            soma += numero;
+        }
+        Some (soma)
     }
-    return Some(lista[0]);
 }
 
 fn main(){
     let lista = vec![1000, 10000, 1000000];
     let vazia: Vec<i32> = vec![];
 
-    print!("{:?}", verificar_primeiro(&lista));
-    print!("{:?}", verificar_primeiro(&vazia));
+    print!("{:?}", somar_lista(&lista));
+    print!("{:?}", somar_lista(&vazia));
 }
