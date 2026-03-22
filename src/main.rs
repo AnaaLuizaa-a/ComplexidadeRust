8bcbb4e6fa8d42972d0b5faefcf7e65c66085090
Ana Luiza <analuizamattos884@gmail.com>
2026-03-22 18:00:14 -0300

    Exercicio 6

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
-pub fn imprimir_pares_e_soma(lista: &[i32], alvo: i32){
-    for i in 0..lista.len(){
-        print!("{}", lista[i]);
-        for i in 0..lista.len(){
-            for j in (i+1).. lista.len(){
-                print!("({}, {}) ", lista[i], lista[j]);
-            }
-        }
-    
+pub fn potencias_de_dois(n: u64){
+    let mut i: u64 = 1;
+    while i< n{
+        print!("{} ", i);
+        i *= 2;
     }
 }
 
 fn main(){
-    let lista = vec![1000, 10000, 1000000];
+    let _lista = vec![1000, 10000, 1000000];
 
-    imprimir_pares_e_soma(&lista, 10000);
+    potencias_de_dois(10000);
 }
