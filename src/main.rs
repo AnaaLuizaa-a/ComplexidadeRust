2972a23e1f124e47ca6cbafcecff71e156df6eae
Ana Luiza <analuizamattos884@gmail.com>
2026-03-22 19:58:30 -0300

    Exercicio 9

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-pub fn ordenacao_bolha(lista: &mut [i32]) {
-    let n = lista.len();
+use std::vec;
+
+pub fn produto_de_matrizes(a: &[Vec<i64>], b: &[Vec<i64>]) -> Vec<Vec<i64>> {
+    let n: usize = a.len();
+    let mut c = vec![vec![0i64;n]; n];
     for i in 0..n {
-        for j in 0..n-i-1 {
-            if lista[j] > lista[j+1] {
-                lista.swap(j,j+1);
+        for j in 0..n{
+            for k in 0..n{
+                c[i][j] += a[i][k] * b[k][j];
             }
         }
     }
+    c
 }
 
 fn main(){
-    let mut lista = vec![1000, 10000, 1000000];
-    ordenacao_bolha(&mut lista);
+    let a = vec![vec![1, 2], vec![3, 4]];
+    let b = vec![vec![5, 6], vec![7, 8]];
+    let c = produto_de_matrizes(&a, &b);
+    println!("{:?}", c);
 }
