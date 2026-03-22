ae18cf60160e8d018c61cbee98ff365724ba7b85
Ana Luiza <analuizamattos884@gmail.com>
2026-03-22 18:39:49 -0300

    Exercicio 7

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-pub fn potencias_de_dois(n: u64){
-    let mut i: u64 = 1;
-    while i< n{
-        print!("{} ", i);
-        i *= 2;
+pub fn fibonacci_recursivo(n: u64) -> u64 {
+    if n <= 1{
+        return n;
+    } else {
+        return fibonacci_recursivo(n - 1) + fibonacci_recursivo(n - 2);
     }
 }
 
 fn main(){
     let _lista = vec![1000, 10000, 1000000];
 
-    potencias_de_dois(10000);
+    println!("Fibonacci de 1000: {}", fibonacci_recursivo(1000));
+    println!("Fibonacci de 10000: {}", fibonacci_recursivo(10000));
+    println!("Fibonacci de 1000000: {}", fibonacci_recursivo(1000000));
 }
