a66ba97c386120b27d386bc168fabd6c3f966b95
Ana Luiza <analuizamattos884@gmail.com>
2026-03-22 19:13:04 -0300

    Exercicio 8

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-pub fn fibonacci_recursivo(n: u64) -> u64 {
-    if n <= 1{
-        return n;
-    } else {
-        return fibonacci_recursivo(n - 1) + fibonacci_recursivo(n - 2);
+pub fn ordenacao_bolha(lista: &mut [i32]) {
+    let n = lista.len();
+    for i in 0..n {
+        for j in 0..n-i-1 {
+            if lista[j] > lista[j+1] {
+                lista.swap(j,j+1);
+            }
+        }
     }
 }
 
 fn main(){
-    let _lista = vec![1000, 10000, 1000000];
-
-    println!("Fibonacci de 1000: {}", fibonacci_recursivo(1000));
-    println!("Fibonacci de 10000: {}", fibonacci_recursivo(10000));
-    println!("Fibonacci de 1000000: {}", fibonacci_recursivo(1000000));
+    let mut lista = vec![1000, 10000, 1000000];
+    ordenacao_bolha(&mut lista);
 }
